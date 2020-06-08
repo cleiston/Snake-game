21d7ac3c906854d11e9349479d7ee380ed294253
Cleiston <cleistonrs@gmail.com>
2020-06-08 15:44:12 -0300

    CSS for the game board

---
+++ snake.js
@@ -0,0 +1,13 @@
+export const SNAKE_SPEED = 1;
+
+const snakeBody = [{x: 11, y: 11}];
+
+export default function update() {
+    console.log('update snake');
+}
+
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
+        
+    });
+}
